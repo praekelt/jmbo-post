fca3f9d4445f796fc22c9f2681ff26045d6714d9
Shaun Sephton <shaunsephton@gmail.com>
2011-09-08 16:43:31 +0200

    empty tests

---
+++ post/tests.py
@@ -0,0 +1,8 @@
+import unittest
+
+from category import admin, models
+
+
+class TestCase(unittest.TestCase):
+    def test_something(self):
+        raise NotImplementedError('Test not implemented. Bad developer!')
