b5d6e101c9ac4b9a26118e8aeb311d13a2adf206
hedleyroos <hedleyroos@gmail.com>
2012-02-21 12:12:26 +0200

    Remove redundant tags and templates

--- post/templatetags/post_inclusion_tags.py
+++ post/templatetags/post_inclusion_tags.py
@@ -3,11 +3,6 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag('post/inclusion_tags/post_listing.html')
-def post_listing(object_list):
-    return {'object_list': object_list}
-
-
 @register.inclusion_tag('post/inclusion_tags/post_detail.html')
 def post_detail(obj):
     return {'object': obj}
