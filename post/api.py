497c326107ef7a2fb4fb1ded1ab1b672fa4ecd7c
hedleyroos <hedleyroos@gmail.com>
2012-06-14 10:17:08 +0200

    Add tastypie API

---
+++ post/api.py
@@ -0,0 +1,12 @@
+from tastypie.resources import ModelResource
+
+from jmbo.api import ModelBaseResource
+
+from post.models import Post
+
+
+class PostResource(ModelBaseResource):
+
+    class Meta:
+        queryset = Post.permitted.all()
+        resource_name = 'post'
