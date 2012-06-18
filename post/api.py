97d27a1256082819821cbf8f018399eeda91be4e
hedleyroos <hedleyroos@gmail.com>
2012-06-18 14:15:20 +0200

    Use slugs

--- post/api.py
+++ post/api.py
@@ -1,5 +1,6 @@
-from tastypie.resources import ModelResource
+from django.conf.urls.defaults import url
 
+from tastypie.resources import ModelResource
 from jmbo.api import ModelBaseResource
 
 from post.models import Post
@@ -10,3 +11,8 @@ class PostResource(ModelBaseResource):
     class Meta:
         queryset = Post.permitted.all()
         resource_name = 'post'
+
+    def override_urls(self):
+        return [
+            url(r"^(?P<resource_name>%s)/(?P<slug>[\w-]+)/$" % self._meta.resource_name, self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
+        ]
