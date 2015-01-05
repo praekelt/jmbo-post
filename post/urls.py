152d9283205b978e142cb3302d60b63255a66e8a
hedleyroos <hedleyroos@gmail.com>
2015-01-05 15:27:16 +0200

    Put in ckeditor note

--- post/urls.py
+++ post/urls.py
@@ -7,6 +7,8 @@ from post.api import PostResource
 
 v1_api.register(PostResource())
 
+# xxx: may need to include ckeditor urls here. check!
+
 urlpatterns = patterns(
     '',
     url(r'^(?P<slug>[\w-]+)/$', 'jmbo.views.object_detail', name='post_object_detail'),
