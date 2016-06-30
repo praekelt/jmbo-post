b824af89f5272d013bbec63c3fcc3dd27f2d87f8
hedleyroos <hedleyroos@gmail.com>
2016-06-30 13:14:48 +0200

    Test the API

--- post/api/__init__.py
+++ post/api/__init__.py
@@ -1,14 +1,34 @@
+from rest_framework.serializers import ReadOnlyField, Serializer
+
 from jmbo import api as jmbo_api
 
 from post.models import Post
 
 
+class PropertiesMixin(Serializer):
+    content = ReadOnlyField()
+    content_pages = ReadOnlyField()
+
+    class Meta:
+        fields = ("content", "content_pages")
+
+
+class HyperlinkedModelBaseSerializer(
+    PropertiesMixin, jmbo_api.HyperlinkedModelBaseSerializer
+    ):
+
+    class Meta(jmbo_api.HyperlinkedModelBaseSerializer.Meta):
+        pass
+
+
 class PostObjectsViewSet(jmbo_api.ModelBaseObjectsViewSet):
     queryset = Post.objects.all()
+    serializer_class = HyperlinkedModelBaseSerializer
 
 
 class PostPermittedViewSet(jmbo_api.ModelBasePermittedViewSet):
     queryset = Post.permitted.all()
+    serializer_class = HyperlinkedModelBaseSerializer
 
 
 def register(router):
