62ed6ffa6ba6546ab74bad11f69d2fab1c0d0844
hedleyroos <hedleyroos@gmail.com>
2016-07-24 20:49:27 +0200

    Get tests to pass

--- post/api/__init__.py
+++ post/api/__init__.py
@@ -32,31 +32,10 @@ class PostPermittedViewSet(jmbo_api.ModelBasePermittedViewSet):
 
 
 def register(router):
-    router.register(
-        r"post-post",
-        PostObjectsViewSet,
-    )
-    router.register(
-        r"post-post-permitted",
-        PostPermittedViewSet,
-    )
-
-def register(router):
-    """Register all viewsets known to post, overriding any items already
-    registered with the same name."""
-
-    for pth, klass in (
-        ("post-post-permitted", PostPermittedViewSet),
-        ("post-post", PostObjectsViewSet),
-    ):
-        keys = [tu[0] for tu in router.registry]
-        try:
-            i = keys.index(pth)
-            del router.registry[i]
-        except ValueError:
-            pass
-        router.register(
-            r"%s" % pth,
-            klass,
-            pth
+    return jmbo_api.register(
+        router,
+        (
+            ("post-post-permitted", PostPermittedViewSet),
+            ("post-post", PostObjectsViewSet)
         )
+    )
