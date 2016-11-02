0a0a337472ff95f17e5912602b932b3ca09dd1ac
hedleyroos <hedleyroos@gmail.com>
2016-11-02 13:14:35 +0200

    Fix url naming

--- post/urls.py
+++ post/urls.py
@@ -7,11 +7,11 @@ urlpatterns = [
     url(
         r"^(?P<category_slug>[\w-]+)/(?P<slug>[\w-]+)/$",
         ObjectDetail.as_view(),
-        name="post-detail"
+        name="post-categorized-detail"
     ),
     url(
         r"^(?P<slug>[\w-]+)/$",
         ObjectDetail.as_view(),
-        name="post-categorized-detail"
+        name="post-detail"
     ),
 ]
